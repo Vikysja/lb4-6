94bb0b79040272a2928c92d5ce4ce59f9d96ca92
Vikysja <91226103+Vikysja@users.noreply.github.com>
2021-11-02 16:38:41 +0200

    Add files via upload

---
+++ lab 4.6.cpp
@@ -0,0 +1,66 @@
+﻿// lab_4.6.cpp
+// Квецко Вікторія
+// Лабораторна робота № 4.6
+// Вкладені цикли
+// Варіант 11
+
+#include <iostream>
+#include <cmath>
+using namespace std;
+int main() {
+    double P, S;
+    int n, k;
+    P = 1;
+    n = 1;
+    while (n <= 25)
+    {
+        S = 0;
+        k = 1;
+        while (k <= n)
+        {
+            P *= sqrt(pow(cos(k + n), 2));
+            k++;
+        }
+        S += cos(n) + P;
+        n++;
+    }
+    cout << P << endl;
+    P = 1;
+    n = 1;
+    do {
+        S = 0;
+        k = 1;
+        do {
+            P *= sqrt(pow(cos(k + n), 2));
+            k++;
+        } while (k <= n);
+        S += cos(n) + P;
+        n++;
+    } while (n <= 25);
+    cout << P << endl;
+    P = 1;
+    for (n = 1; n <= 25; n++)
+    {
+        S = 0;
+        for (k = 1; k <= n; k++)
+        {
+            P *= sqrt(pow(cos(k + n), 2));
+        }
+        S += cos(n) + P;
+    }
+    cout << P << endl;
+    P = 1;
+    for (n = 25; n >= 1; n--)
+    {
+        S = 0;
+        for (k = n; k >= 1; k--)
+
+        {
+            P *= sqrt(pow(cos(k + n), 2));
+
+        }
+        S += cos(n) + P;
+    }
+    cout << P << endl;
+    return 0;
+}
